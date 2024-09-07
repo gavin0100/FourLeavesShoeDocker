28f352e0bf4996f3f19dcf5b91ded0af8e37297f
VoVanDuc20110635 <20110635@student.hcmute.edu.vn>
2024-09-07 09:38:33 +0700

    fix AccessDeniedProcession 3

--- src/main/java/com/data/filtro/config/CustomErrorHandler.java
+++ src/main/java/com/data/filtro/config/CustomErrorHandler.java
@@ -11,22 +11,23 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 
 import java.io.IOException;
 
-//public class CustomErrorHandler implements AccessDeniedHandler, AuthenticationEntryPoint {
-public class CustomErrorHandler implements AuthenticationEntryPoint {
+public class CustomErrorHandler implements AccessDeniedHandler, AuthenticationEntryPoint {
+//public class CustomErrorHandler implements AuthenticationEntryPoint {
     // handle là của AccessDeniedHandler, được thực thi nếu ng dùng không có quyền,
     // lúc này request đã đi qua hầu hết bộ lọc filter, bị kẹt lại ở AuthorizationFilter chỉ còn bước lấy Authentication
     // từ SecurityContextHolder lên để so sáng với @PreAuthorized nhưng không khớp
-//    @Override
-//    public void handle(HttpServletRequest request, HttpServletResponse response,
-//                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
-//        response.sendRedirect("/access-denied");
-//    }
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        System.out.println("CustomErrorHandler AccessDeniedHandler");
+        response.sendRedirect("/");
+    }
 
     // commence là của AuthenticationEntryPoint, được thực thi nếu ng dùng chưa được xác thực
     // mà muốn thực hiện request cần PreAuthorized
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        System.out.println("AuthenticationEntryPoint được gọi");
+        System.out.println("CustomErrorHandler AuthenticationEntryPoint");
         response.sendRedirect("/access-denied");
     }
 }
